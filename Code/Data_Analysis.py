4bf6cfc49c3e3e64971de280eab66cf64a7bc5cc
drvinceknight <vincent.knight@gmail.com>
2012-11-15 09:45:20 +0000

    Changed scatter plot in to IM map.

--- Code/Data_Analysis.py
+++ Code/Data_Analysis.py
@@ -15,39 +15,39 @@ infile.close
 instances=0
 wins=0
 number=len(data)
-scatter_m=[]
-scatter_n=[]
-max_data={}
 running_win_percentage=[]
+max_m=0
+max_n=0
+number_of_wins_dictionary={}
 for row in data:
     instances+=1
     m=eval(row[1])
     n=eval(row[2])
-    if m in max_data:
-        max_data[m]=max(max_data[m],n)
-    else:
-        max_data[m]=n
+    string="[%s,%s]"%(m,n)
+    max_m=max(max_m,m)
+    max_n=max(max_n,n)
     if eval(row[0]):
         wins+=1
-        scatter_m.append(m)
-        scatter_n.append(n)
+        if string in number_of_wins_dictionary:
+            number_of_wins_dictionary[string]+=1
+        else:
+            number_of_wins_dictionary[string]=1
     running_win_percentage.append(wins/instances)
 
-line_m=[]
-line_maxn=[]
 
-for key in max_data:
-    line_m.append(key)
-    line_maxn.append(max_data[key])
+number_of_wins_array=[[0 for n in range(max_n)] for m in range(max_m)]
+for e in number_of_wins_dictionary:
+    l=eval(e)
+    number_of_wins_array[l[0]-1][l[1]-1]=number_of_wins_dictionary[e]
 
 plt.figure()
-plt.imshow([[1,2],[2,3]])
-plt.plot(line_m,line_maxn,label="Region Explored",color="black")
-plt.scatter(scatter_m,scatter_n, label="Wins (%s/%s)"%(wins,number))
+plt.imshow(number_of_wins_array,origin='lower',interpolation='nearest')
+plt.colorbar()
+plt.set_cmap('hot')
 plt.xlabel("m")
 plt.ylabel("n")
 plt.title("Location of wins")
-plt.legend(loc="upper right")
+#plt.legend(loc="upper right")
 plt.savefig("Scatter_plot_of_wins.pdf")
 
 plt.figure()
